ad07969d116bd4406b79b2b89f9288fc9bee9a36
2015244007_yun <46712186+yunhc99@users.noreply.github.com>
2021-02-19 15:15:38 +0900

    Update app.js
    대대적 테스트 수정

--- app.js
+++ app.js
@@ -1,42 +1,19 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-
-var app = http.createServer(function(request, response) {
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if (_url == '/') {
-        title = 'Welcome';
-    }
-    if (_url == '/favicon.ico') {
-        return response.writeHead(404);
-    }
-    response.writeHead(200);
-    var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-      <img src="coding.jpg" width="100%">
-      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
-      </p>
-    </body>
-    </html>
-    `;
-    response.end(template);
-
+const http = require('http');
+ 
+const hostname = '0.0.0.0';
+const port = '9999';
+ 
+// http모듈의 createServer 함수를 호출로 서버 생성
+// req: 웹 요청 매개변수, res: 웹 응답 매개변수
+httpd = http.createServer(function (req, res) {
+    // writeHead: 응답 헤더를 작성
+    // 200: 응답 성공, text/html: html문서
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    // end: 응답 본문을 작성
+    res.end('Hello World111');   
 });
-app.listen(3000);
-console.log("port 3000 start");
+
+// listen: 매개변수로 포트와 호스트를 지정
+httpd.listen(port, hostname, function(){    
+    console.log('server start');
+});
